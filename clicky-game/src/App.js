a3be63235f916b42d7198c852a2fa60d723662d9
David Cabrera <dcabrera1982@gmail.com>
2019-10-27 21:56:53 -0400

    push to heroku

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -99,7 +99,7 @@ class App extends Component {
     render() {
         return (
             <Wrapper>
-                <Title>Clickity Clack Movie Game</Title>
+                <Title>Clicky Clicky</Title>
         
                 <h3 className="scoreSummary">
                     {this.state.clickMessage}
